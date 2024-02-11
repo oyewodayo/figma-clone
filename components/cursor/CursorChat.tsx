ed4c2758263749e2fe64602e74fba107a40ab38f
Oyewo Temidayo <oyewodayo@gmail.com>
2024-02-11 15:38:49 +0100

    Added Navigation bar and users

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -34,7 +34,7 @@ const CursorChat = ({cursor, cursorState, setCursorState, updateMyPresence}:Curs
         <>
           <CursorSVG color='#000' />
 
-          <div className='absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[15px]'>
+          <div className='absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[15px]' onKeyUp={(e)=>e.stopPropagation}>
             
             {cursorState.previousMessage && (
               <div> {cursorState.previousMessage} </div>
---
+++ components/users/Avatar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Avatar.module.css";
+
+const IMAGE_SIZE = 48;
+
+export function Avatar({ name, otherStyles }: { otherStyles: string; name: string }) {
+  return (
+    <div className={`${styles.avatar} ${otherStyles} h-9 w-9`} data-tooltip={name}>
+      <img
+        src={`https://liveblocks.io/avatars/avatar-${Math.floor(Math.random()*30)}.png`}
+        height={IMAGE_SIZE}
+        width={IMAGE_SIZE}
+        className={styles.avatar_picture}
+      />
+    </div>
+  );
+}
